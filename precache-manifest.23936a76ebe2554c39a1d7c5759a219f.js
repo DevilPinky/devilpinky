d4ea39759cc8dc1ca57f5752575b270397126fb1
David Babayan <babayan.dav@gmail.com>
2019-02-06 20:30:31 +0100

    Updates

---
+++ precache-manifest.23936a76ebe2554c39a1d7c5759a219f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "825d756b207d0f2432f8",
+    "url": "/devilpinky/static/css/main.ad091d0e.chunk.css"
+  },
+  {
+    "revision": "825d756b207d0f2432f8",
+    "url": "/devilpinky/static/js/main.825d756b.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/devilpinky/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "f579f44e2e2072f1617c",
+    "url": "/devilpinky/static/js/runtime~main.f579f44e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/devilpinky/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "806628cc088b4b38ff6b764e5673d2f1",
+    "url": "/devilpinky/index.html"
+  }
+];
